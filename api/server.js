cd8cbc9359fb3ced1ef41ab90678d31bc7c94996
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-26 15:47:26 -0400

    added router to server

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
+
+const carRouter = require("../cars/carsRouter");
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.status(200).json({ api: "up"})
-})
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.use("/api/car", carRouter);
 
 module.exports = server;
